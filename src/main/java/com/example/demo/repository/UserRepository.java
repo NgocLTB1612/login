91b467001feeaa0e455006df81db5aed6fb4a7a5
lC7qg8zbkf <ngocltb@vnext.com.vn>
2021-02-23 23:52:06 +0700

    security

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+}
---
+++ src/main/java/com/example/demo/service/MessageResponse.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+public class MessageResponse {
+    public MessageResponse(String s) {
+
+    }
+}
